463eef98773704fdccd728e8ce9f0fdf72e146a0
GM Ubungen <gmubungen@gmail.com>
2023-08-06 23:17:52 +0800

    Update: done moving the model

--- sequelize/index.js
+++ sequelize/index.js
@@ -4,6 +4,22 @@ const db = {};
 
 const sequelize = new Sequelize(config);
 
+fs.readdirSync(__dirname)
+  .filter((file) => file !== "index.js")
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if ("associate" in db[modelName]) {
+    db[modelName].associate(db);
+  }
+});
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
